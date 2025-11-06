dd26e3df3af076de850cde5d19dc0871103cbbf8
Christian C A George <christiancageorge@gmail.com>
2025-11-06 14:22:12 +0100

    Add 6-multi_langauges_loop.js script using array and loop

---
+++ 6-multi_languages_loop.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+
+// Create an array of strings
+const languages = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+
+// Initialize index
+let i = 0;
+
+// Use a loop to go through the array
+while (i < languages.length) {
+  console.log(languages[i]);
+  i++;
+}
